6566321feffad87942c885291d533957cf66d613
Dinoork <dinoork29@gmail.com>
2021-04-07 01:16:12 +0300

    docs

--- docs/Landing wineshop.651df39b.js
+++ docs/Landing wineshop.651df39b.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";var e=require("./select/select");require("./select/style.scss");var l=new e.Select("#select",{placeholder:"Choose me",selectedId:"1",data:[{id:"1",value:"Бутик на Невском 103"},{id:"2",value:"Новокузнечный переулок 4/1"},{id:"3",value:"Московский проспект 14"},{id:"4",value:"Бутик на  Лиговском 89"},{id:"5",value:"улица Рубинштейна 32"},{id:"6",value:"Кронверкский проспект 57"}],onSelect:function(e){console.log("Selected Item",e)}});window.s=l;
 },{"./select/select":"FRWC","./select/style.scss":"Sp1x"}]},{},["Focm"], null)
-//# sourceMappingURL=/index.js.map
+//# sourceMappingURL=/Landing%20wineshop.651df39b.js.map
